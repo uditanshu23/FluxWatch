2db089b14c560fc4101db255111f1c2c9c1e2950
Uditanshu Satpathy <uditanshusatpathy23@gmail.com>
2026-04-26 22:07:26 +0530

    Innitial Commit

---
+++ src/main/java/com/example/FluxWatch/FluxWatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.FluxWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxWatchApplication.class, args);
+	}
+
+}
